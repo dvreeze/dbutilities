7fc10dc863d3d5e4f0150508d7399662d036f387
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-03-07 16:33:18 +0100

    Added GetTableColumnsMetaData

---
+++ src/main/java/eu/cdevreeze/dbutilities/connectionfunction/GetTableColumnsMetaData.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.dbutilities.connectionfunction;
+
+import eu.cdevreeze.dbutilities.ConnectionToJsonObjectFunction;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonValue;
+import jakarta.json.spi.JsonProvider;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+/**
+ * {@link ConnectionToJsonObjectFunction} that retrieves metadata of a table's columns in the database.
+ * <p>
+ * Instances are created by a dedicated factory object, and not by CDI injection.
+ *
+ * @author Chris de Vreeze
+ */
+public class GetTableColumnsMetaData implements ConnectionToJsonObjectFunction {
+
+    private final String tableName;
+
+    public GetTableColumnsMetaData(String tableName) {
+        this.tableName = tableName;
+    }
+
+    @Override
+    public JsonObject apply(Connection connection) {
+        // Unlike Json, JsonProvider does not involve a lookup each time it is used
+        JsonProvider jsonProvider = JsonProvider.provider();
+        try {
+            DatabaseMetaData dbMetaData = connection.getMetaData();
+            try (ResultSet rs = dbMetaData.getColumns(null, null, tableName, null)) {
+                JsonArrayBuilder tablesJsonArr = jsonProvider.createArrayBuilder();
+                while (rs.next()) {
+                    tablesJsonArr.add(
+                            jsonProvider.createObjectBuilder()
+                                    .add(
+                                            "tableCat",
+                                            Optional.ofNullable((JsonValue) jsonProvider.createValue(rs.getString("TABLE_CAT")))
+                                                    .orElse(JsonValue.NULL)
+                                    )
+                                    .add(
+                                            "tableSchema",
+                                            Optional.ofNullable((JsonValue) jsonProvider.createValue(rs.getString("TABLE_SCHEM")))
+                                                    .orElse(JsonValue.NULL)
+                                    )
+                                    .add("tableName", rs.getString("TABLE_NAME"))
+                                    .add("columnName", rs.getString("COLUMN_NAME"))
+                                    .add("dataType", rs.getInt("DATA_TYPE"))
+                                    .add("typeName", rs.getString("TYPE_NAME"))
+                                    .add("columnSize", rs.getInt("COLUMN_SIZE"))
+                                    .add("decimalDigits", Optional.ofNullable(rs.getString("DECIMAL_DIGITS"))
+                                            .map(Integer::parseInt).map(v -> (JsonValue) jsonProvider.createValue(v)).orElse(JsonValue.NULL))
+                                    .add("numPrecRadix", Optional.ofNullable(rs.getString("NUM_PREC_RADIX"))
+                                            .map(Integer::parseInt).map(v -> (JsonValue) jsonProvider.createValue(v)).orElse(JsonValue.NULL))
+                                    .add("nullable", Optional.ofNullable(rs.getString("NULLABLE"))
+                                            .map(Integer::parseInt).map(v -> (JsonValue) jsonProvider.createValue(v)).orElse(JsonValue.NULL))
+                                    .add(
+                                            "remarks",
+                                            Optional.ofNullable((JsonValue) jsonProvider.createValue(rs.getString("REMARKS")))
+                                                    .orElse(JsonValue.NULL)
+                                    )
+                                    .add(
+                                            "columnDef",
+                                            Optional.ofNullable((JsonValue) jsonProvider.createValue(rs.getString("COLUMN_DEF")))
+                                                    .orElse(JsonValue.NULL)
+                                    )
+                                    .add("charOctetLength", Optional.ofNullable(rs.getString("CHAR_OCTET_LENGTH"))
+                                            .map(Integer::parseInt).map(v -> (JsonValue) jsonProvider.createValue(v)).orElse(JsonValue.NULL))
+                                    .add("ordinalPosition", Optional.ofNullable(rs.getString("ORDINAL_POSITION"))
+                                            .map(Integer::parseInt).map(v -> (JsonValue) jsonProvider.createValue(v)).orElse(JsonValue.NULL))
+                                    .add("isNullable", rs.getString("IS_NULLABLE"))
+                                    .add("isAutoIncrement", rs.getString("IS_AUTOINCREMENT"))
+                                    .add("isGeneratedColumn", rs.getString("IS_GENERATEDCOLUMN"))
+                    );
+                }
+                return jsonProvider.createObjectBuilder()
+                        .add("columns", tablesJsonArr)
+                        .build();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
