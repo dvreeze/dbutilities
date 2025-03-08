411ccdbe7fa2a0f06b623caa3d20f3274f183999
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-03-08 10:20:07 +0100

    Added SelectAllFromTableAsXml

---
+++ src/main/java/eu/cdevreeze/dbutilities/connectionfunction/SelectAllFromTableAsXml.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.dbutilities.connectionfunction;
+
+import com.google.common.collect.ImmutableList;
+import eu.cdevreeze.dbutilities.ConnectionToElementFunction;
+import eu.cdevreeze.yaidom4j.dom.immutabledom.Element;
+import eu.cdevreeze.yaidom4j.dom.immutabledom.Nodes;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import static eu.cdevreeze.yaidom4j.dom.immutabledom.Nodes.elem;
+
+/**
+ * {@link ConnectionToElementFunction} that selects all data from a given table.
+ * <p>
+ * Instances are created by a dedicated factory object, and not by CDI injection.
+ *
+ * @author Chris de Vreeze
+ */
+public class SelectAllFromTableAsXml implements ConnectionToElementFunction {
+
+    private final String tableName;
+
+    public SelectAllFromTableAsXml(String tableName) {
+        this.tableName = tableName;
+    }
+
+    @Override
+    public Element apply(Connection connection) {
+        // TODO Protect against SQL injection
+
+        try (PreparedStatement ps = connection.prepareStatement("select * from " + tableName)) {
+            try (ResultSet rs = ps.executeQuery()) {
+                List<Element> rows = new ArrayList<>();
+                ResultSetMetaData rsMetaData = rs.getMetaData();
+                while (rs.next()) {
+                    List<Element> columns = new ArrayList<>();
+                    IntStream.rangeClosed(1, rsMetaData.getColumnCount())
+                            .forEach(i -> {
+                                try {
+                                    columns.add(
+                                            elem(rsMetaData.getColumnLabel(i))
+                                                    .plusChildOption(
+                                                            Optional.ofNullable(rs.getString(i))
+                                                                    .map(Nodes::text)
+                                                    )
+                                    );
+                                } catch (SQLException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+                    rows.add(
+                            elem("row").withChildren(ImmutableList.copyOf(columns))
+                    );
+                }
+                return elem("rows").withChildren(ImmutableList.copyOf(rows));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/main/java/eu/cdevreeze/dbutilities/console/JdbcProgramReturningXml.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.dbutilities.console;
+
+import com.google.common.base.Preconditions;
+import eu.cdevreeze.dbutilities.ConnectionToElementFunction;
+import eu.cdevreeze.dbutilities.ConnectionToElementFunctionFactory;
+import eu.cdevreeze.yaidom4j.dom.immutabledom.Element;
+import eu.cdevreeze.yaidom4j.dom.immutabledom.jaxpinterop.DocumentPrinter;
+import eu.cdevreeze.yaidom4j.dom.immutabledom.jaxpinterop.DocumentPrinters;
+import jakarta.enterprise.inject.Default;
+import jakarta.enterprise.inject.Instance;
+import jakarta.enterprise.inject.literal.NamedLiteral;
+import jakarta.enterprise.inject.spi.CDI;
+import org.eclipse.microprofile.config.Config;
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Console program using a {@link ConnectionToElementFunction}. The first program argument
+ * is the name of the {@link ConnectionToElementFunction} to create and run,
+ * and the remaining program arguments are passed to the {@link ConnectionToElementFunctionFactory}
+ * to create a {@link ConnectionToElementFunction}, which is subsequently run.
+ *
+ * @author Chris de Vreeze
+ */
+public class JdbcProgramReturningXml {
+
+    public static void main(String... args) {
+        Objects.checkIndex(0, args.length);
+        String connectionFunctionName = args[0];
+        List<String> factoryArgs = Arrays.stream(args).skip(1).toList();
+
+        Weld weld = new Weld();
+
+        try (WeldContainer weldContainer = weld.initialize()) {
+            Instance<Config> configInstance = CDI.current().select(Config.class, Default.Literal.INSTANCE);
+
+            Preconditions.checkArgument(
+                    configInstance.isResolvable(),
+                    String.format("Could not resolve Config with required qualifier '%s'", Default.Literal.INSTANCE)
+            );
+
+            Config config = configInstance.get();
+
+            // Typically, system property "dataSourceName" has been passed to the program
+            String dataSourceName =
+                    config.getOptionalValue("dataSourceName", String.class).orElseThrow();
+
+            Instance<DataSource> dataSourceInstance = CDI.current().select(DataSource.class, NamedLiteral.of(dataSourceName));
+
+            Preconditions.checkArgument(
+                    dataSourceInstance.isResolvable(),
+                    String.format("Could not resolve DataSource with name '%s'", dataSourceName)
+            );
+
+            Instance<ConnectionToElementFunctionFactory> functionFactoryInstance =
+                    CDI.current().select(ConnectionToElementFunctionFactory.class, NamedLiteral.of(connectionFunctionName));
+
+            Preconditions.checkArgument(
+                    functionFactoryInstance.isResolvable(),
+                    String.format("Could not resolve function with name '%s'", connectionFunctionName)
+            );
+
+            ConnectionToElementFunction function = functionFactoryInstance.get().apply(factoryArgs);
+
+            // Do the actual work within a JDBC Connection
+            Element result;
+            try (Connection conn = dataSourceInstance.get().getConnection()) {
+                result = function.apply(conn);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            DocumentPrinter docPrinter = DocumentPrinters.instance();
+            String xmlString = docPrinter.print(result);
+
+            System.out.println(xmlString);
+        }
+    }
+}
