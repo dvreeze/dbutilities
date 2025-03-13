0ecde00630d0b25cb3d8ce2ffaae5a017cde4754
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-03-13 21:19:36 +0100

    Added support for arbitrary queries

---
+++ src/main/java/eu/cdevreeze/dbutilities/connectionfunction/GetQueryResults.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.dbutilities.connectionfunction;
+
+import eu.cdevreeze.dbutilities.ConnectionToJsonObjectFunction;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * {@link ConnectionToJsonObjectFunction} that returns the results of a query as JSON.
+ * <p>
+ * Instances are created by a dedicated factory object, and not by CDI injection.
+ *
+ * @author Chris de Vreeze
+ */
+public class GetQueryResults extends AbstractGetQueryResults {
+
+    private final Path queryFile;
+
+    public GetQueryResults(Path queryFile) {
+        this.queryFile = queryFile;
+    }
+
+    // TODO Protect against SQL injection
+
+    @Override
+    protected String getQueryString() {
+        try {
+            return Files.readString(queryFile).strip();
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+}
---
+++ src/main/java/eu/cdevreeze/dbutilities/console/GetQueryResultsAsXmlProgram.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.dbutilities.console;
+
+import eu.cdevreeze.dbutilities.connectionfunction.GetQueryResultsAsXml;
+
+import java.nio.file.Path;
+import java.util.Objects;
+
+/**
+ * Program that calls {@link GetQueryResultsAsXml} and shows the result.
+ * <p>
+ * The only program argument is the query file path.
+ *
+ * @author Chris de Vreeze
+ */
+public class GetQueryResultsAsXmlProgram {
+
+    public static void main(String... args) {
+        Objects.checkIndex(0, args.length);
+        Path queryFile = Path.of(args[0]);
+
+        JdbcProgramReturningXml.main(
+                GetQueryResultsAsXml.class.getSimpleName(),
+                queryFile.toString()
+        );
+    }
+}
