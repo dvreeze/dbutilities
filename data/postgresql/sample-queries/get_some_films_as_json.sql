e32c996d6faa96899e46b487fafff49701725dc7
Chris de Vreeze <chris.de.vreeze@caiway.net>
2025-03-15 01:25:30 +0100

    Support for JSON-returning queries

---
+++ data/postgresql/sample-queries/get_some_films_as_json.sql
@@ -0,0 +1,32 @@
+select json_object(
+           'film': json_object(
+               'film_id': film.film_id,
+               'title': film.title,
+               'description': film.description,
+               'release_year': film.release_year,
+               'language_id': film.language_id,
+               'rental_duration': film.rental_duration,
+               'rental_rate': film.rental_rate,
+               'length': film.length,
+               'replacement_cost': film.replacement_cost,
+               'rating': film.rating,
+               'last_update': film.last_update,
+               'special_features': array_to_string(film.special_features, ', '),
+               'fulltext': film.fulltext,
+               'actors': json_agg(
+                   json_object('actor': json_object(
+                           'actor_id': film_actor.actor_id,
+                           'film_actor_last_update': film_actor.last_update,
+                           'actor_first_name': actor.first_name,
+                           'actor_last_name': actor.last_name,
+                           'actor_last_update': actor.last_update
+                       )
+                   )
+               )
+           )
+       )
+  from film
+  left join film_actor on (film.film_id = film_actor.film_id)
+  left join actor on (film_actor.actor_id = actor.actor_id)
+ where upper(film.title) like 'WA%'
+ group by film.film_id
---
+++ src/main/java/eu/cdevreeze/dbutilities/connectionfunction/GetJsonQueryResults.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2025-2025 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.dbutilities.connectionfunction;
+
+import com.google.common.base.Preconditions;
+import eu.cdevreeze.dbutilities.ConnectionToJsonObjectFunction;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonReaderFactory;
+import jakarta.json.JsonString;
+import jakarta.json.spi.JsonProvider;
+
+import java.io.StringReader;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.util.Map;
+
+/**
+ * {@link ConnectionToJsonObjectFunction} that returns the results of a JSON-returning query as JSON.
+ * It is expected that each row has just one column, and that it is of type "JSON object".
+ * <p>
+ * Instances are created by a dedicated factory object, and not by CDI injection.
+ *
+ * @author Chris de Vreeze
+ */
+public class GetJsonQueryResults implements ConnectionToJsonObjectFunction {
+
+    private final GetQueryResults delegate;
+
+    public GetJsonQueryResults(Path queryFile) {
+        this.delegate = new GetQueryResults(queryFile);
+    }
+
+    // TODO Protect against SQL injection
+
+    @Override
+    public JsonObject apply(Connection connection) {
+        JsonObject rawResult = delegate.apply(connection);
+
+        Preconditions.checkArgument(rawResult.containsKey("rows"));
+        Preconditions.checkArgument(rawResult.get("rows") instanceof JsonArray);
+        JsonArray rows = rawResult.get("rows").asJsonArray();
+
+        Preconditions.checkArgument(rows.stream().allMatch(v -> v.asJsonObject().containsKey("json_object")));
+
+        JsonProvider jsonProvider = JsonProvider.provider();
+        JsonReaderFactory jsonReaderFactory = jsonProvider.createReaderFactory(Map.of());
+
+        return jsonProvider.createObjectBuilder()
+                .add(
+                        "rows",
+                        jsonProvider.createArrayBuilder(
+                                rows.stream()
+                                        .map(row -> (JsonString) row.asJsonObject().get("json_object"))
+                                        .map(escapedJson -> jsonReaderFactory.createReader(new StringReader(escapedJson.getString())).readObject())
+                                        .toList()
+                        )
+                )
+                .build();
+    }
+}
